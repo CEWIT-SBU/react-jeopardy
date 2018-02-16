aed9602912414951fe6bc5c5398b722fd2b7930d
Matthew Cordaro <cordaro.matthew@gmail.com>
2018-02-15 21:44:48 -0500

    Additional stuff Misset in 'Lots of Changes' Commit

--- double-jeopardy-round.js
+++ double-jeopardy-round.js
@@ -10,13 +10,153 @@ const data = [
         ]
     },
     {
-        "category": "FINAL JEOPARDY",
+        "category": "HEY, THAT'S MY PI",
         "questions": [
             {
-                "points": "MAJOR LEAGUE HACKING",
-                "question": "THIS IS THE YEAR MLH WAS FOUNDED",
-                "answer": "2013"
+                "points": 200,
+                "question": "THIS POPULAR CREDIT CARD SIZED DEVICE THAT CAN DO MANY OF THE THINGS THAT A DESKTOP PC DOES IS TYPICALLY USED IN ELECTRONICS PROJECTS",
+                "answer": "WHAT IS A RASPBERRY PI?"
             },
+            {
+                "points": 400,
+                "question": "THE NUMBER OF PINS AN EXTENDED GPIO HAS, SUCH AS ON THE PI3.",
+                "answer": "WHAT IS 40?"
+            },
+            {
+                "points": 600,
+                "question": "APPEND THIS TO A COMMAND TO EXECUTE IT WITH ROOT PRIVILEGE",
+                "answer": "WHAT IS SUDO?"
+            },
+            {
+                "points": 800,
+                "question": "THIS IS WHAT 'NOOBS' STARNDS FOR",
+                "answer": "WHAT IS NEW OUT OF BOX SOFTWARE?"
+            },
+            {
+                "points": 1000,
+                "question": "THE DEFAULT USERNAME FOLLOWED BY THE DEFAULT PASSWORD",
+                "answer": "WHAT IS PI RASPBERRY?"
+            }
         ]
     },
+    {
+        "category": "ERGO, A.I.",
+        "questions": [
+            {
+                "points": 200,
+                "question": "THIS IBM SYSTEM COMPETED ON JEOPARDY! AGAINST HUMAN EXPERTS BRAD RUTTER AND KEN JENNINGS WINNING THE FIRST PLACE PRIZE OF $1 MILLION.",
+                "answer": "WHAT IS WATSON?"
+            },
+            {
+                "points": 400,
+                "question": "THIS FREE ONLINE ENCYCLOPEDIA HAS THE MISSION OF ALLOWING ANYONE TO CREATE OR EDIT ARTICLES",
+                "answer": "WHAT IS WIKIPEDIA?"
+            },
+            {
+                "points": 600,
+                "question": "THIS PROJECT EXTRACTED STRUCTURED CONTENT FROM THE INFORMATION CREATED IN THE WIKIPEDIA PROJECT",
+                "answer": "WHAT IS DBPEDIA?"
+            },
+            {
+                "points": 800,
+                "question": "THIS ENGLISH ENGINEER AND COMPUTER SCIENTIST IS BEST KNOWN AS THE INVENTOR OF THE WORLD WIDE WEB",
+                "answer": "WHO IS TIM BERNERS-LEE?"
+            },
+            {
+                "points": 1000,
+                "question": "THIS PRINCETON DEVELOPED LEXICAL DATABASE GROUPS WORDS INTO SETS OF SYNONYMS CALLED SYNSETS, PROVIDES DEFINITIONS, AND OTHER RELATIONS AMONG SYNONYM SETS (SUCH AS HYPERNYMS, HOLONYMS, MERONYMS, GLOSS, ETC.)",
+                "answer": "WHAT IS WORDNET?"
+            }
+        ]
+    },
+    {
+        "category": "SOFTHEON UI",
+        "questions": [
+            {
+                "points": 200,
+                "question": "USE THIS TO STYLE ELEMENTS AS AN ALTERNATIVE TO TRADITIONAL CSS CLASSES",
+                "answer": "WHAT ARE ATTRIBUTES? / WHAT ARE SMART ATTRIBUTES?"
+            },
+            {
+                "points": 400,
+                "question": "THIS IS WHAT ITS BUILT WITH",
+                "answer": "WHAT IS CSS? / WHAT IS SASS?"
+            },
+            {
+                "points": 600,
+                "question": "THE NAMING CONVENTION",
+                "answer": "WHAT IS BEM? / WHAT IS BLOCK – ELEMENT – MODIFIER?"
+            },
+            {
+                "points": 800,
+                "question": "THIS IS THE COLOR PALETTE USED",
+                "answer": "WHAT IS FLAT UI COLORS?"
+            },
+            {
+                "points": 1000,
+                "question": "THE ICON IN THE SOFTHEON WORKSHOP UI LOGO",
+                "answer": "WHAT IS AN ANVIL?"
+            }
+        ]
+    },
+    {
+        "category": "ARDUIKNOW!",
+        "questions": [
+            {
+                "points": 200,
+                "question": "THE SECTION YOU WOULD PLACE YOUR INITIALIZATION CODE IN",
+                "answer": "WHAT IS VOID SETUP OR SETUP SECTION?"
+            },
+            {
+                "points": 400,
+                "question": "THE WIFI MODULE FOUND IN THE NODEMCU",
+                "answer": "WHAT IS ESP8266?"
+            },
+            {
+                "points": 600,
+                "question": "THIS IS WHERE WOULD YOU GO IN THE ARDUINO IDE TO ADD A NEW BOARD FILE",
+                "answer": "WHAT IS THE BOARD MANAGER?"
+            },
+            {
+                "points": 800,
+                "question": "PWM STAND FOR THIS",
+                "answer": "WHAT IS PULSE WIDTH MODULATION?"
+            },
+            {
+                "points": 1000,
+                "question": "HE CREATED ARDUINO",
+                "answer": "WHO IS MASSIMO BANZI?"
+            }
+        ]
+    },
+    {
+        "category": "IT'S O'K'",
+        "questions": [
+            {
+                "points": 200,
+                "question": "THE SECTION YOU WOULD PLACE YOUR INITIALIZATION CODE IN",
+                "answer": "WHAT IS VOID SETUP OR SETUP SECTION?"
+            },
+            {
+                "points": 400,
+                "question": "THE WIFI MODULE FOUND IN THE NODEMCU",
+                "answer": "WHAT IS ESP8266?"
+            },
+            {
+                "points": 600,
+                "question": "THIS IS WHERE WOULD YOU GO IN THE ARDUINO IDE TO ADD A NEW BOARD FILE",
+                "answer": "WHAT IS THE BOARD MANAGER?"
+            },
+            {
+                "points": 800,
+                "question": "PWM STAND FOR THIS",
+                "answer": "WHAT IS PULSE WIDTH MODULATION?"
+            },
+            {
+                "points": 1000,
+                "question": "HE CREATED ARDUINO",
+                "answer": "WHO IS MASSIMO BANZI?"
+            }
+        ]
+    }
 ];
