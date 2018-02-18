0c5e94146ca25f2321097d531e1e42ca0e5368a2
Matthew Cordaro <cordaro.matthew@gmail.com>
2018-02-17 21:23:31 -0500

    Fix double Jeopardy

--- double-jeopardy-round.js
+++ double-jeopardy-round.js
@@ -13,27 +13,27 @@ const data = [
         "category": "HEY, THAT'S MY PI",
         "questions": [
             {
-                "points": 200,
+                "points": 400,
                 "question": "THIS IS THE DEFAULT PASSWORD",
                 "answer": "WHAT IS RASPBERRY?"
             },
             {
-                "points": 400,
+                "points": 800,
                 "question": "THE NUMBER OF PINS AN EXTENDED GPIO HAS, SUCH AS ON THE PI3",
                 "answer": "WHAT IS 40?"
             },
             {
-                "points": 600,
+                "points": 1200,
                 "question": "THIS IS WHAT 'NOOBS' STANDS FOR",
                 "answer": "WHAT IS NEW OUT OF BOX SOFTWARE?"
             },
             {
-                "points": 800,
+                "points": 1600,
                 "question": "THE TWO VOLTAGES FOUND ON THE GPIO HEADER",
                 "answer": "WHAT ARE 3.3 AND 5?"
             },
             {
-                "points": 1000,
+                "points": 2000,
                 "question": "TWO OF THE THREE COMMON SERIAL BUSES (I²C, SPI, & UART)",
                 "answer": "WHAT IS THE INTER-INTEGRATED CIRCUIT, THE SERIAL PERIPHERAL INTERFACE, AND THE UNIVERSAL ASYNCHRONOUS RECEIVER/TRANSMITTER?"
             }
@@ -43,27 +43,27 @@ const data = [
         "category": "ETHICAL H4CK1N6",
         "questions": [
             {
-                "points": 200,
+                "points": 400,
                 "question": "THIS TOOL IS USED TO MONITOR EVERY KEYSTROKE",
                 "answer": "WHAT IS A KEYLOGGER?"
             },
             {
-                "points": 400,
+                "points": 800,
                 "question": "THIS MALWARE TYPE WAS USED IN RANSOMWARE FOR HOSPITALS AND WANNACRY",
                 "answer": "WHAT IS A ROOTKIT?"
             },
             {
-                "points": 600,
+                "points": 1200,
                 "question": "THIS TECHNIQUE IS USED TO GAIN INFORMATION ABOUT A COMPUTER SYSTEM AND THE SERVICES RUNNING ON ITS OPEN PORTS",
                 "answer": "WHAT IS BANNER GRABBING?"
             },
             {
-                "points": 800,
+                "points": 1600,
                 "question": "THIS IS THE EASIEST MEAN OF CRACKING A PASSWORD",
                 "answer": "WHAT IS SOCIAL ENGINEERING?"
             },
             {
-                "points": 1000,
+                "points": 2000,
                 "question": "THIS IS WHERE THE COLOR \"BLUE\" ORIGINATED FROM IN \"BLUE HAT HACKER\"",
                 "answer": "WHAT IS MICROSOFT BADGES COLOR?"
             }
@@ -73,27 +73,27 @@ const data = [
         "category": "JAVASCRIPT ON THE SERVER?!",
         "questions": [
             {
-                "points": 200,
+                "points": 400,
                 "question": "THIS COMMON OPEN SOURCE SERVER FRAMEWORK BASED ON JAVASCRIPT RUNS ON VARIOUS PLATFORMS INCLUDING WINDOWS, LINUX, UNIX & MAC OS X",
                 "answer": "WHAT IS NODEJS?"
             },
             {
-                "points": 400,
+                "points": 800,
                 "question": "WITH OVER 465,000 MODULES THIS HAS BECOME THE LARGEST ECOSYSTEM OF OPEN SOURCE LIBRARIES IN THE WORLD - BE SPECIFIC",
                 "answer": "WHAT IS NODE PACKADE MANAGER?"
             },
             {
-                "points": 600,
+                "points": 1200,
                 "question": "THIS LIGHT FRAMEWORK WHICH HAS BECOME THE DEFACTO STANDARD WEB SERVER FOR NODEJS APPS, HELPS MANAGE ROUTES, HANDLING REQUESTS, AND VIEWS",
                 "answer": "WHAT IS EXPRESS?"
             },
             {
-                "points": 800,
+                "points": 1600,
                 "question": "AN OPEN SOURCE JAVASCRIPT ENGINE THAT WAS DEVELOPED BY THE CHROMIUM PROJECT AND DESIGNED TO BE USED BOTH IN BROWSER AND IN STANDALONE IMPLEMENTATIONS",
                 "answer": "WHAT IS CHROME V8?"
             },
             {
-                "points": 1000,
+                "points": 2000,
                 "question": "A C++ API THAT PROVIDES AN ABSTRACTION LAYER FOR NETWORK AND FILE SYSTEM FUNCTIONALITY ON BOTH WINDOWS AND POSIX-BASED SYSTEMS",
                 "answer": "WHAT IS LIBUV?"
             }
@@ -103,27 +103,27 @@ const data = [
         "category": "ERGO, AI",
         "questions": [
             {
-                "points": 200,
+                "points": 400,
                 "question": "THIS IBM SYSTEM COMPETED ON JEOPARDY! AGAINST HUMAN EXPERTS BRAD RUTTER AND KEN JENNINGS WINNING THE FIRST PLACE PRIZE OF $1 MILLION",
                 "answer": "WHAT IS WATSON?"
             },
             {
-                "points": 400,
+                "points": 800,
                 "question": "THIS FREE ONLINE ENCYCLOPEDIA HAS THE MISSION OF ALLOWING ANYONE TO CREATE OR EDIT ARTICLES",
                 "answer": "WHAT IS WIKIPEDIA?"
             },
             {
-                "points": 600,
+                "points": 1200,
                 "question": "THIS PROJECT EXTRACTED STRUCTURED CONTENT FROM THE INFORMATION CREATED IN THE WIKIPEDIA PROJECT",
                 "answer": "WHAT IS DBPEDIA?"
             },
             {
-                "points": 800,
+                "points": 1600,
                 "question": "THIS ENGLISH ENGINEER AND COMPUTER SCIENTIST IS BEST KNOWN AS THE INVENTOR OF THE WORLD WIDE WEB",
                 "answer": "WHO IS TIM BERNERS-LEE?"
             },
             {
-                "points": 1000,
+                "points": 2000,
                 "question": "THIS PRINCETON DEVELOPED LEXICAL DATABASE GROUPS WORDS INTO SETS OF SYNONYMS CALLED SYNSETS, PROVIDES DEFINITIONS, AND OTHER RELATIONS AMONG SYNONYM SETS (SUCH AS HYPERNYMS, HOLONYMS, MERONYMS, GLOSS, ETC)",
                 "answer": "WHAT IS WORDNET?"
             }
@@ -133,30 +133,30 @@ const data = [
         "category": "WHAT'S UP, DOCKER?",
         "questions": [
             {
-                "points": 200,
+                "points": 400,
                 "question": "THESE ARE STORED IN THE REGISTRY",
                 "answer": "WHAT ARE DOCKER IMAGES?"
             },
             {
-                "points": 400,
+                "points": 800,
                 "question": "WHILE VIRTUAL MACHINES USE HYPERVISORS, THE DOCKER ARCHITECTURE USES THIS",
                 "answer": "WHAT IS THE DOCKER ENGINE?"
             },
             {
-                "points": 600,
-                "question": "THE LANGUAGE IN WHICH DOCKER IS WRITTEN",
-                "answer": "WHAT IS GOLANG?"
+                "points": 800,
+                "question": "THIS DOCKER COMMAND IS USED TO GET INTO A DOCKER CONTAINER",
+                "answer": "WHAT IS EXEC?"
             },
             {
-                "points": 800,
+                "points": 1600,
                 "question": "THE COMPANY THAT ORIGINALLY DESIGNED KUBERNETES",
                 "answer": "WHAT IS GOOGLE?"
             },
             {
-                "points": 1000,
-                "question": "THIS AUTOMATICALLY SCALES DOCKER IMAGES UP AND DOWN IN KUBERNETES",
-                "answer": "WHAT IS HORIZONTAL POD AUTOSCALING?"
-            }
+                "points": 2000,
+                "question": "THE LANGUAGE IN WHICH DOCKER IS WRITTEN",
+                "answer": "WHAT IS GOLANG?"
+            },
         ]
     },
 ];
